448ff66eea7586e1cec23ac6b49e2ad45b9f0340
fflopsi <florian.l.frauenfelder@gmail.com>
2024-01-05 18:49:32 +0100

    Add missing packages for TeXLive minted package

--- machines/flopsi-thinkpad-nix/home.nix
+++ machines/flopsi-thinkpad-nix/home.nix
@@ -20,6 +20,7 @@
   # Packages to be installed
   home.packages = with pkgs; [
     onlyoffice-bin
+    python3 python311Packages.pygments
     (texlive.combine { inherit (texlive) scheme-medium minted; })
     texstudio unstable.typst
     unstable.obsidian rnote xournalpp
