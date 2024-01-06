f50d11767f7937c1224b396c4cfafb7312440435
fflopsi <florian.l.frauenfelder@gmail.com>
2024-01-06 19:05:16 +0100

    Add some GNOME settings #3

--- modules/home-manager/gnome-dconf.nix
+++ modules/home-manager/gnome-dconf.nix
@@ -1,53 +1,58 @@
-{ ... }:
+{ lib, ... }:
 
+with lib.hm.gvariant;
 {
   # Add script to change Gnome theme quickly
   home.file.".local/bin/change-theme".source = ../../files/change-theme;
 
   # Dconf settings for Gnome
-  dconf.settings = {
+  dconf.settings = let keybinds = "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings"; in {
     "org/gnome/settings-daemon/plugins/media-keys" = {
       calculator = [ "<Super>c" ];
       control-center = [ "<Super>i" ];
-      custom-keybindings = [ "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/" "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/" "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/" "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/" "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom4/" "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom5/" ];
+      custom-keybindings = [ "/${keybinds}/custom0/" "/${keybinds}/custom1/" "/${keybinds}/custom2/" "/${keybinds}/custom3/" "/${keybinds}/custom4/" "/${keybinds}/custom5/" ];
       home = [ "<Super>e" ];
       www = [ "<Super>b" ];
     };
 
-    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+    "${keybinds}/custom0" = {
       binding = "<Control><Alt>0";
       command = "change-theme";
       name = "Change theme";
     };
 
-    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
+    "${keybinds}/custom1" = {
       binding = "<Control><Alt>k";
       command = "secrets";
       name = "Gnome Secrets";
     };
 
-    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2" = {
+    "${keybinds}/custom2" = {
       binding = "<Control><Super>r";
       command = "reboot";
       name = "Reboot";
     };
 
-    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3" = {
+    "${keybinds}/custom3" = {
       binding = "<Shift><Control>Escape";
       command = "kgx -- btop";
       name = "System monitor";
     };
 
-    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom4" = {
+    "${keybinds}/custom4" = {
       binding = "<Control><Alt>Return";
       command = "kgx";
       name = "Terminal";
     };
 
-    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom5" = {
+    "${keybinds}/custom5" = {
       binding = "<Control><Super>l";
       command = "gnome-session-quit";
       name = "Logout";
+    };
+
+    "system/locale" = {
+      region = "de_CH.UTF-8";
     };
 
     "org/gnome/shell" = {
@@ -57,6 +62,8 @@
     "org/gnome/desktop/interface" = {
       monospace-font-name = "Terminess Nerd Font Mono Regular 14";
       icon-theme = "Papirus";
+      color-scheme = "prefer-dark";
+      gtk-theme = "adw-gtk3-dark";
       enable-hot-corners = false;
       clock-show-weekday = true;
       clock-show-seconds = true;
@@ -79,6 +86,16 @@
 
     "org/gnome/Console" = {
       theme = "auto";
+    };
+
+    "org/gnome/TextEditor" = {
+      indent-style = "space";
+      tab-width = mkUint32 2;
+    };
+
+    "org/gnome/calculator" = {
+      button-mode = "advanced";
+      show-thousands = true;
     };
 
     "org/gnome/desktop/calendar" = {
