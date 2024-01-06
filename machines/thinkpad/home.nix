be9346d1819928ec6d6e23594323ff3ad8d582a4
fflopsi <florian.l.frauenfelder@gmail.com>
2024-01-06 21:05:45 +0100

    Add extension settings #2

--- machines/thinkpad/home.nix
+++ machines/thinkpad/home.nix
@@ -4,6 +4,7 @@
   imports = let modules = "../../modules/home-manager"; in [
     ./${modules}/basic-home.nix
     ./${modules}/gnome-dconf.nix
+    ./${modules}/gnome-extensions.nix
     ./${modules}/office.nix
     ./${modules}/coding.nix
     ./${modules}/bash.nix
---
+++ modules/home-manager/gnome-extensions.nix
@@ -0,0 +1,23 @@
+{ ... }:
+
+{
+  dconf.settings = {
+    "org/gnome/shell" = {
+      enabled-extensions = [ "appindicatorsupport@rgcjonas.gmail.com" "clipboard-indicator@tudmotu.com" "gnome-ui-tune@itstime.tech" "gsconnect@andyholmes.github.io" "quick-settings-tweaks@qwreey" ];
+    };
+
+    "org/gnome/shell/extensions/clipboard-indicator" = {
+      move-item-first = true;
+    };
+
+    "org/gnome/shell/extensions/gnome-ui-tune" = {
+      always-show-thumbnails = false;
+      hide-search = false;
+    };
+
+    "/org/gnome/shell/extensions/quick-settings-tweaks" = {
+      add-dnd-quick-toggle-enabled = false;
+      user-removed-buttons = [ "RfkillToggle" "KeyboardBrightnessToggle" "DarkModeToggle" "NightLightToggle" ];
+    };
+  };
+}
