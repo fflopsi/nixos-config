2267cc768c634f67914bcc7f20d3c589a8f274d3
fflopsi <florian.l.frauenfelder@gmail.com>
2024-06-30 13:37:09 +0200

    Add hyprland config

---
+++ modules/home-manager/home-hyprland.nix
@@ -0,0 +1,345 @@
+{ config, osConfig, pkgs, ... }:
+
+{
+  imports = [
+    ./office.nix
+    ./coding.nix
+    ./bash.nix
+  ];
+
+  home.username = "flopsi";
+  home.homeDirectory = "/home/flopsi";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "23.05";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # Packages to be installed
+  home.packages = with pkgs; [
+    terminus-nerdfont papirus-icon-theme adw-gtk3
+    kitty gnome.nautilus wofi gnome-text-editor
+    brightnessctl pamixer playerctl networkmanagerapplet grimblast xdg-desktop-portal-hyprland
+    hypridle wl-clipboard
+  ];
+
+  # Add ~/.local/bin to path (for user-specific scripts)
+  home.sessionPath = [ "$HOME/.local/bin" ];
+
+  wayland.windowManager.hyprland = {
+    enable = true;
+    settings = {
+      monitor = ",preferred,auto,1";
+      "$mod" = "SUPER";
+      exec-once = [
+        "waybar & (sleep 5 && megasync)"
+        "hypridle"
+        "blueman-applet"
+        "nm-applet"
+        "wl-paste --watch cliphist store"
+        "kdeconnect-indicator"
+      ];
+      env = [
+        "XCURSOR_SIZE, 24"
+        "HYPRCURSOR_SIZE, 24"
+      ];
+      bind = [
+        "$mod, B, exec, firefox"
+        "$mod, R, exec, pidof wofi && pkill wofi || wofi -S run"
+        "$mod, T, exec, gnome-text-editor"
+        ", Print, exec, grimblast copy area"
+        "$mod, V, exec, pidof wofi && pkill wofi || cliphist list | wofi -S dmenu | cliphist decode | wl-copy"
+        "CONTROL ALT, return, exec, kitty"
+        "$mod, S, exec, systemctl suspend"
+        "CONTROL ALT $mod, delete, exec, poweroff"
+        "CONTROL $mod, R, exec, reboot"
+        "$mod, Q, killactive,"
+        "$mod, M, exit,"
+        "$mod, L, exec, loginctl lock-session"
+        "$mod, E, exec, nautilus"
+        "$mod, F, togglefloating,"
+        "$mod, P, pseudo,"
+        "$mod, J, togglesplit,"
+        "$mod, left, movefocus, l"
+        "$mod, right, movefocus, r"
+        "$mod, up, movefocus, u"
+        "$mod, down, movefocus, d"
+        "$mod, tab, cyclenext,"
+        "ALT, tab, cyclenext,"
+        "$mod SHIFT, tab, cyclenext, prev"
+        "ALT SHIFT, tab, cyclenext, prev"
+        "$mod, mouse_down, workspace, e+1"
+        "$mod, mouse_up, workspace, e-1"
+        "CONTROL ALT, right, workspace, e+1"
+        "CONTROL ALT, left, workspace, e-1"
+        ", XF86AudioRaiseVolume, exec, pamixer -i 5"
+        ", XF86AudioLowerVolume, exec, pamixer -d 5"
+        ", XF86AudioMute, exec, pamixer -t"
+        ", XF86AudioPlay, exec, playerctl play-pause"
+        ", XF86AudioNext, exec, playerctl next"
+        ", XF86AudioPrev, exec, playerctl previous"
+        ", XF86MonBrightnessUp, exec, brightnessctl s +5%"
+        ", XF86MonBrightnessDown, exec, brightnessctl s 5%-"
+      ]
+      ++ (
+        # workspaces
+        # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
+        builtins.concatLists (builtins.genList (
+          x: let
+            ws = let
+              c = (x + 1) / 10;
+            in
+              builtins.toString (x + 1 - (c * 10));
+          in [
+            "$mod, ${ws}, workspace, ${toString (x + 1)}"
+            "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+          ]
+        )
+        10)
+      );
+      bindr = [
+        "$mod, Super_L, exec, pidof wofi && pkill wofi || wofi -S drun"
+      ];
+      bindl = [
+        ",switch:on:Lid Switch, exec, systemctl suspend"
+      ];
+      bindm = [
+        "$mod, mouse:272, movewindow"
+        "$mod, CONTROL_L, movewindow"
+        "$mod, mouse:273, resizewindow"
+        "$mod, ALT_L, resizewindow"
+      ];
+      general = {
+        gaps_in = 5;
+        gaps_out = 5;
+        border_size = 1;
+        "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+        "col.inactive_border" = "rgba(595959aa)";
+        resize_on_border = true;
+        allow_tearing = false;
+        layout = "dwindle";
+      };
+      decoration = {
+        rounding = 10;
+        active_opacity = 1.0;
+        inactive_opacity = 1.0;
+        drop_shadow = true;
+        shadow_range = 4;
+        shadow_render_power = 3;
+        "col.shadow" = "rgba(1a1a1aee)";
+        blur = {
+          enabled = true;
+          size = 3;
+          passes = 1;
+          vibrancy = 0.1696;
+        };
+      };
+      dwindle = {
+        pseudotile = true;
+        preserve_split = true;
+      };
+      master = {
+        new_status = "master";
+      };
+      misc = {
+        force_default_wallpaper = -1;
+        disable_hyprland_logo = false;
+      };
+      input = {
+        kb_layout = "us";
+        kb_variant = "altgr-intl";
+        kb_options = "lv3:ralt_switch";
+        follow_mouse = 1;
+        accel_profile = "flat";
+        sensitivity = 0.5;
+        touchpad = {
+            natural_scroll = true;
+        };
+      };
+      gestures = {
+        workspace_swipe = true;
+      };
+      windowrulev2 = [
+        "float, title:\\.blueman-manager-wrapped$"
+        "move 100%-w-6 40, title:\\.blueman-manager-wrapped$"
+        "float, title:Network Connections$"
+        "suppressevent maximize, class:.*"
+      ];
+    };
+  };
+
+  home.file.".config/hypr/hyprlock-bg.png".source = ../../files/nix-wallpaper-gear.png;
+
+  programs.hyprlock = {
+    enable = true;
+    settings = {
+      general = {
+        disable_loading_bar = false;
+        grace = 0;
+      };
+      background = [
+        {
+          path = "$HOME/.config/hypr/hyprlock-bg.png";
+          blur_passes = 2;
+          blur_size = 8;
+        }
+      ];
+      input-field = [
+        {
+          size = "250, 50";
+          position = "0, -150";
+          monitor = "";
+          dots_center = true;
+          fade_on_empty = true;
+          font_color = "rgb(202, 211, 245)";
+          inner_color = "rgb(91, 96, 120)";
+          outer_color = "rgb(24, 25, 38)";
+          outline_thickness = 2;
+          placeholder_text = "Password";
+          shadow_passes = 2;
+        }
+      ];
+    };
+  };
+
+  services.hypridle = {
+    enable = true;
+    settings = {
+      general = {
+        lock_cmd = "pidof hyprlock || hyprlock";
+        before_sleep_cmd = "loginctl lock-session";
+        after_sleep_cmd = "hyprctl dispatch dpms on";
+      };
+      listener = [
+        {
+          timeout = 150;
+          on-timeout = "brightnessctl -s set 7";
+          on-resume = "brightnessctl -r";
+        }
+        {
+          timeout = 300;
+          on-timeout = "loginctl lock-session";
+        }
+        {
+          timeout = 330;
+          on-timeout = "hyprctl dispatch dpms off";
+          on-resume = "hyprctl dispatch dpms on";
+        }
+        {
+          timeout = 600;
+          on-timeout = "systemctl suspend";
+        }
+      ];
+    };
+  };
+
+  services.dunst = {
+    enable = true;
+    settings = {
+      global = {
+        width = 300;
+        height = 300;
+        offset = "20x20";
+        origin = "top-right";
+        transparency = 10;
+        #frame_color = "#eceff1";
+        font = "Droid Sans 10";
+      };
+    };
+  };
+
+  services.cliphist = {
+    enable = true;
+  };
+
+  programs.waybar = {
+    enable = true;
+    settings = {
+      mainBar = {
+        layer = "top";
+        position = "top";
+        height = 34;
+        spacing = 16;
+        modules-left = [ "hyprland/workspaces" "hyprland/window" ];
+        modules-center = [ "clock" ];
+        modules-right = [ "backlight" "pulseaudio" "bluetooth" "network" "battery" "tray" ];
+        "hyprland/window" = {
+          icon = true;
+        };
+        clock = {
+          format = "{:%a, %d. %b %Y     %T}";
+          interval = 1;
+          tooltip-format = "{calendar}";
+          actions = {
+            on-click = "mode";
+          };
+          calendar = {
+            weeks-pos = "left";
+          };
+        };
+        battery = {
+          states = {
+            warning = 25;
+            critical = 10;
+          };
+        };
+        backlight = {
+          format = "🔆 {percent}%";
+        };
+        network = {
+          format-wifi = "{essid} ({signalStrength}%)";
+          format-ethernet = "Cable";
+          format-disconnected = "?";
+          tooltip-format-wifi = "{frequency} GHz, {bandwidthTotalBytes} on {ipaddr}";
+          tooltip-format-ethernet = "{bandwidthTotalBytes} on {ipaddr}";
+          tooltip-format-disconnected = "Disconnected";
+          interval = 10;
+        };
+        bluetooth = {
+          format-connected = " {status}: {device_alias}";
+          tooltip-format-connected = "{status} to {device_enumerate}";
+          tooltip-format-enumerate-connected = "{device_alias}";
+        };
+        pulseaudio = {
+          format-muted = "Muted: {volume}%";
+          on-click = "pamixer -t";
+        };
+        tray = {
+          spacing = 12;
+        };
+      };
+    };
+    style = ''
+      #workspaces button.active {
+        background-color: darkgreen;
+      }
+      #battery.warning {
+        color: black;
+        background-color: darkorange;
+      }
+      #battery.critical {
+        background-color: darkred;
+      }
+      #network.wifi {
+        background-color: darkslateblue;
+      }
+      #network.ethernet {
+        background-color: darkgreen;
+      }
+      #network.disconnected {
+        color: black;
+        background-color: darkorange;
+      }
+      #network.disabled {
+        background-color: dimgrey;
+      }
+    '';
+  };
+}
---
+++ modules/nixos/configuration-hyprland.nix
@@ -0,0 +1,127 @@
+{ config, pkgs, ... }:
+
+{
+  nix = {
+    settings.experimental-features = [ "nix-command" "flakes" ]; # Enable flakes
+    optimise.automatic = true; # Optimise nix store by hardlinking
+    gc = {
+      automatic = true; # Automate garbage collection
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
+  };
+
+  # Bootloader
+  boot.loader = {
+    efi.canTouchEfiVariables = true;
+    systemd-boot = {
+      # Use systemd-boot as bootloader
+      enable = true;
+      configurationLimit = 5;
+    };
+  };
+
+  # Set time zone
+  time.timeZone = "Europe/Zurich";
+
+  # Select internationalisation properties
+  i18n = {
+    defaultLocale = "en_US.UTF-8"; # System language
+    extraLocaleSettings = { # Formats
+      LC_ADDRESS = "de_CH.UTF-8";
+      LC_IDENTIFICATION = "de_CH.UTF-8";
+      LC_MEASUREMENT = "de_CH.UTF-8";
+      LC_MONETARY = "de_CH.UTF-8";
+      LC_NAME = "de_CH.UTF-8";
+      LC_NUMERIC = "de_CH.UTF-8";
+      LC_PAPER = "de_CH.UTF-8";
+      LC_TELEPHONE = "de_CH.UTF-8";
+      LC_TIME = "de_CH.UTF-8";
+    };
+  };
+
+  # X11 windowing system
+  services.xserver = {
+    enable = true;
+    # Enable touchpad support (enabled default in most desktopManager)
+    # libinput.enable = true;
+  };
+
+  # Hyprland
+  programs.hyprland.enable = true;
+
+  networking.networkmanager.enable = true;
+
+  services.logind = {
+    powerKey = "suspend";
+    lidSwitch = "suspend";
+  };
+  security.pam.services.hyprlock = {};
+
+  # Enable CUPS to print documents
+  services.printing.enable = true;
+  # Enable printer specific fixes
+  services.avahi.enable = true;
+  services.avahi.nssmdns4 = true;
+
+  # Enable sound with pipewire
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable bluetooth
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
+  services.blueman.enable = true;
+
+  # Define user account
+  users.users.flopsi = {
+    isNormalUser = true;
+    description = "Florian Frauenfelder";
+    extraGroups = [ "networkmanager" "wheel" ];
+  };
+
+  # List packages installed in system profile
+  environment.systemPackages = with pkgs; [
+    topgrade
+    firefox thunderbird libreoffice
+    micro btop tldr gitFull inetutils wget curl sl
+  ];
+
+  # Enable flatpak
+  services.flatpak.enable = true;
+
+  #programs.kdeconnect = {
+  #  enable = true;
+  #  package = pkgs.kdePackages.kdeconnect-kde;
+  #};
+
+  # Default apps
+  xdg.mime.defaultApplications = {
+    "application/pdf" = "firefox.desktop";
+  };
+
+  # Sudo insults
+  security.sudo.package = pkgs.sudo.override { withInsults = true; };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+}
