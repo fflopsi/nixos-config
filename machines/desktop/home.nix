21c8c4859f9a5450f9a1dec6ec9b8637a9825d07
fflopsi <florian.l.frauenfelder@gmail.com>
2024-07-19 19:15:24 +0200

    Add workspace config

--- machines/desktop/home.nix
+++ machines/desktop/home.nix
@@ -7,8 +7,22 @@
   ];
 
   # Displays
-  wayland.windowManager.hyprland.settings.monitor = [
-    "DP-2,2560x1440@59.95,0x0,1"
-    "DP-1,2560x1440@279.96,2560x0,1,vrr,2"
-  ];
+  wayland.windowManager.hyprland.settings = {
+    monitor = [
+      "DP-2,2560x1440@59.95,0x0,1"
+      "DP-1,2560x1440@279.96,2560x0,1,vrr,2"
+    ];
+    workspace = (
+      builtins.concatLists (builtins.genList (
+        x: let ws = builtins.toString (x+1);
+        in [ "${ws}, monitor:DP-1, persistent:true" ]
+      ) 5)
+    )
+    ++ (
+      builtins.concatLists (builtins.genList (
+        x: let ws = builtins.toString (x+6);
+        in [ "${ws}, monitor:DP-2, persistent:true" ]
+      ) 4)
+    );
+  };
 }
