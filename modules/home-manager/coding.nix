50f1d5c138cec90017e5c9a0d3a244c6d1f0e629
fflopsi <florian.l.frauenfelder@gmail.com>
2024-09-19 23:50:16 +0200

    Add programs, VS Code settings

--- modules/home-manager/coding.nix
+++ modules/home-manager/coding.nix
@@ -1,24 +1,35 @@
 { pkgs, ... }:
 
 {
-  # Packages to be installed
   home.packages = with pkgs; [
     android-studio
     jetbrains.idea-community
     jetbrains.pycharm-community
     jetbrains.pycharm-professional
-    vscodium
     git-credential-oauth
   ];
 
-  # Git
-  programs.git = {
-    enable = true;
-    userName  = "fflopsi";
-    userEmail = "florian.l.frauenfelder@gmail.com";
-    extraConfig = {
-      init.defaultBranch = "main";
-      credential.helper = "oauth";
+  programs = {
+    gh.enable = true;
+    gh-dash.enable = true;
+    gradle.enable = true;
+    java.enable = true;
+    lazygit.enable = true;
+
+    git = {
+      enable = true;
+      userName  = "fflopsi";
+      userEmail = "florian.l.frauenfelder@gmail.com";
+      extraConfig = {
+        init.defaultBranch = "main";
+        credential.helper = "oauth";
+      };
+    };
+
+    vscode = {
+      enable = true;
+      package = pkgs.vscodium;
+      extensions = [ pkgs.vscode-extensions.bbenoist.nix ];
     };
   };
 }
