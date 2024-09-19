c342859266c95f962aaa851b09c9a30915290bfd
fflopsi <florian.l.frauenfelder@gmail.com>
2024-09-19 23:49:07 +0200

    Add editorconfig, Gnome Text Editor settings, programs and mime defaults

--- modules/home-manager/office.nix
+++ modules/home-manager/office.nix
@@ -1,15 +1,15 @@
 { pkgs, ... }:
 
 {
-  # Packages to be installed
   home.packages = with pkgs; [
+    gnome-text-editor
     onlyoffice-bin
     python3 python311Packages.pygments
     (texlive.combine { inherit (texlive) scheme-medium minted cancel wrapfig tabularray enumitem xpatch datetime2 datetime2-english datetime2-german; })
     texstudio unstable.typst
     unstable.obsidian xournalpp pandoc
     geogebra6 speedcrunch
-    megasync rclone
+    rclone
     tigervnc vlc spotify
   ];
 
@@ -21,4 +21,44 @@
     publicShare = null;
     videos = null;
   };
+
+  editorconfig = {
+    enable = true;
+    settings = {
+      "*" = {
+        charset = "utf-8";
+        end_of_line = "lf";
+        trim_trailing_whitespace = true;
+        insert_final_newline = true;
+        max_line_width = 78;
+        indent_style = "space";
+        indent_size = 4;
+      };
+      "*.nix" = {
+        indent_size = 2;
+      };
+    };
+  };
+
+  dconf.settings = {
+    "org/gnome/TextEditor" = {
+      discover-settings = true;
+      highlight-current-line = true;
+      indent-style = "space";
+      restore-session = true;
+      show-line-numbers = true;
+      show-map = true;
+      style-variant = "dark";
+      tab-width = mkUint32 2;
+    };
+  };
+
+  programs = {
+    feh.enable = true;
+    yt-dlp.enable = true;
+  };
+
+  services.megasync.enable = true;
+
+  xdg.mimeApps.defaultApplications."application/pdf" = "firefox.desktop";
 }
