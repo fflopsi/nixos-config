fd2365f6758b516dcda45e9d8077e30e4ba91b7f
fflopsi <florian.l.frauenfelder@gmail.com>
2024-09-19 23:51:05 +0200

    Add programs

--- modules/home-manager/bash.nix
+++ modules/home-manager/bash.nix
@@ -1,42 +1,46 @@
 { pkgs, ... }:
 
 {
-  # Bash
-  programs.bash = {
-    enable = true;
-    historyControl = [ "ignoredups" "erasedups" ];
-    historyIgnore = [ "exit" "clear" ];
-    shellAliases = {
-      ll = "ls -l";
-      la = "ls -lA";
-      gedit = "gnome-text-editor";
-      ls = "ls --color=auto";
-      diff = "diff --color=auto";
-      grep = "grep --color=auto";
-      ip = "ip --color=auto";
-    };
-    bashrcExtra = ''
-      mkdircd () { mkdir -p $1 && cd $1; }
-      rebuild () { git -C /etc/nixos fetch && git -C /etc/nixos pull && sudo nixos-rebuild switch; }
-    '';
-  };
+  programs = {
+    autojump.enable = true;
+    broot.enable = true;
+    fzf.enable = true;
+    pls.enable = true;
 
-  # Readline config for bash history search
-  programs.readline = {
-    enable = true;
-    includeSystemConfig = true;
-    bindings = {
-      "\\e[A" = "history-search-backward";
-      "\\e[B" = "history-search-forward";
+    bash = {
+      enable = true;
+      historyControl = [ "ignoredups" "erasedups" ];
+      historyIgnore = [ "exit" "clear" ];
+      shellAliases = {
+        ll = "ls -l";
+        la = "ls -lA";
+        gedit = "gnome-text-editor";
+        ls = "ls --color=auto";
+        diff = "diff --color=auto";
+        grep = "grep --color=auto";
+        ip = "ip --color=auto";
+      };
+      bashrcExtra = ''
+        mkdircd () { mkdir -p $1 && cd $1; }
+        rebuild () { git -C /etc/nixos fetch && git -C /etc/nixos pull && sudo nixos-rebuild switch; }
+      '';
     };
-  };
 
-  # Oh my Posh prompt
-  programs.oh-my-posh = {
-    enable = true;
-    package = pkgs.unstable.oh-my-posh;
-    enableBashIntegration = true;
-    #useTheme = "powerlevel10k_lean";
-    settings = import ./omp-theme.nix;
+    # Readline config for bash history search
+    readline = {
+      enable = true;
+      includeSystemConfig = true;
+      bindings = {
+        "\\e[A" = "history-search-backward";
+        "\\e[B" = "history-search-forward";
+      };
+    };
+
+    oh-my-posh = {
+      enable = true;
+      package = pkgs.unstable.oh-my-posh;
+      #useTheme = "powerlevel10k_lean";
+      settings = import ./omp-theme.nix;
+    };
   };
 }
